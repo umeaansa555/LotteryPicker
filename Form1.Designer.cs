e69a3752d2ab4d31260d6f070625d704ebfea026
P4 <umeaansa555@ed.amdsb.ca>
2021-11-11 13:19:34 -0500

    variables

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -42,6 +42,7 @@ namespace LotteryPicker
             this.button1.TabIndex = 0;
             this.button1.Text = "Generate";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // outputLabel
             // 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,23 @@ namespace LotteryPicker
 {
     public partial class Form1 : Form
     {
+        Random randGen = new Random();
+        int num1;
+        int num2;
+        int num3;
+        int num4;
+        int num5;
+        int num6;
+        int max = 100;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
